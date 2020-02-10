de272663c30f323ea1349a92edbd47713aeb9149
yxj0312 <yanxiajun@hotmail.com>
2020-02-10 10:55:05 +0100

    Add vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import router from './routes'
 
+Vue.use(VueRouter);
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+
+  router
 }).$mount('#app')
