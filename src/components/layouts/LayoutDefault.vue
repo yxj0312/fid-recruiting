67f1937fe25a73ba80f80f12136a96058c6d8912
yxj0312 <yanxiajun@hotmail.com>
2020-02-11 23:58:54 +0100

    Add css modules

--- src/components/layouts/LayoutDefault.vue
+++ src/components/layouts/LayoutDefault.vue
@@ -1,23 +1,24 @@
 <template>
-  <div class="LayoutDefault">
-    <nav class="LayoutDefault__nav">
+  <div class="layout-default">
+    <nav class="layout-default__nav">
       <router-link to="/">Product List</router-link> |
       <router-link to="/about">About</router-link>
 
-      <div class="LayoutDefault__user">
+      <div class="layout-default__user">
         {{ user ? user.name : `Loading...` }}
       </div>
     </nav>
-    <main class="LayoutDefault__main">
+    <main class="layout-default__main">
       <slot/>
     </main>
-    <footer class="LayoutDefault__footer">
+    <footer class="layout-default__footer">
       &copy; Awesome Company
     </footer>
   </div>
 </template>
 
 <script>
+
 export default {
   name: `LayoutDefault`,
   data() {
@@ -35,28 +36,28 @@ export default {
 </script>
 
 <style lang="scss">
-.LayoutDefault {
-  max-width: 42em;
-  margin-right: auto;
-  margin-left: auto;
-  padding-right: 1em;
-  padding-left: 1em;
-  &__nav {
-    padding-top: 1em;
-    padding-bottom: 1em;
-    border-bottom: 1px solid #c0c0c0;
-  }
-  &__user {
-    float: right;
-  }
-  &__main {
-    padding-top: 1.5em;
-    padding-bottom: 2em;
-  }
-  &__footer {
-    padding-top: 1em;
-    padding-bottom: 1em;
-    border-top: 1px solid #c0c0c0;
-  }
-}
+// .LayoutDefault {
+//   max-width: 42em;
+//   margin-right: auto;
+//   margin-left: auto;
+//   padding-right: 1em;
+//   padding-left: 1em;
+//   &__nav {
+//     padding-top: 1em;
+//     padding-bottom: 1em;
+//     border-bottom: 1px solid #c0c0c0;
+//   }
+//   &__user {
+//     float: right;
+//   }
+//   &__main {
+//     padding-top: 1.5em;
+//     padding-bottom: 2em;
+//   }
+//   &__footer {
+//     padding-top: 1em;
+//     padding-bottom: 1em;
+//     border-top: 1px solid #c0c0c0;
+//   }
+// }
 </style>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import routes from './routes'
 Vue.use(VueRouter);
 Vue.config.productionTip = false
 
+import './assets/sass/app.scss';
 
 new Vue({
   render: h => h(App),
