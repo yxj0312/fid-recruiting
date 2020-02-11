ca186f45ae877dc4d3de7b187506a76fbcd851a7
yxj0312 <yanxiajun@hotmail.com>
2020-02-11 20:40:15 +0100

    Install axios

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import routes from './routes'
+import axios from 'axios';
 
 Vue.use(VueRouter);
 Vue.config.productionTip = false
+window.axios = axios;
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 new Vue({
   render: h => h(App),
