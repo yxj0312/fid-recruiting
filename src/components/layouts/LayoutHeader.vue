9785793783f23394f4b33cf8a3475615daf8a9a8
yxj0312 <yanxiajun@hotmail.com>
2020-02-13 15:55:27 +0100

    Add header nav styles

--- src/components/layouts/LayoutHeader.vue
+++ src/components/layouts/LayoutHeader.vue
@@ -3,7 +3,7 @@
         <div class="header-logo">
             <img src="../../assets/logo/logo-peek-und-cloppenburg.03c0.svg" alt="">
         </div>
-        <nav>
+        <nav class="header-nav">
             <ul>
                 <li><router-link to="/">Product List</router-link></li>
                 <li><router-link to="/about">About</router-link></li>
