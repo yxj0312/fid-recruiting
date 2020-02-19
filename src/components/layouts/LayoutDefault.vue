21848d64440442cdab04aa3ff986e1dbcfb2f904
yxj0312 <yanxiajun@hotmail.com>
2020-02-19 22:43:12 +0100

    Add responsive design to layout-main

--- src/components/layouts/LayoutDefault.vue
+++ src/components/layouts/LayoutDefault.vue
@@ -36,30 +36,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss">
-// .LayoutDefault {
-//   max-width: 42em;
-//   margin-right: auto;
-//   margin-left: auto;
-//   padding-right: 1em;
-//   padding-left: 1em;
-//   &__nav {
-//     padding-top: 1em;
-//     padding-bottom: 1em;
-//     border-bottom: 1px solid #c0c0c0;
-//   }
-//   &__user {
-//     float: right;
-//   }
-//   &__main {
-//     padding-top: 1.5em;
-//     padding-bottom: 2em;
-//   }
-//   &__footer {
-//     padding-top: 1em;
-//     padding-bottom: 1em;
-//     border-top: 1px solid #c0c0c0;
-//   }
-// }
-</style>
