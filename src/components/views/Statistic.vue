bd8bdb7b726c1b26b3ce0742493381208f1d539c
yxj0312 <yanxiajun@hotmail.com>
2020-02-21 23:04:04 +0100

    Calculate two works

--- src/components/views/Statistic.vue
+++ src/components/views/Statistic.vue
@@ -33,17 +33,51 @@ import Layout from '../layouts/Layout';
                 this.originalProducts = [];
             })
             .then(({data}) => {
-            this.products = data;
-            this.originalProducts = data;
+                this.products = data;
+                this.originalProducts = data;
             })
         },
 
         computed: {
-            
+            priceLessThanFourty() {
+                return this.products.filter(product => {
+                    return this.getPrice(product) < 40
+                }).map(product => product.brand)
+            },
+
+            countedBrand() {
+                return [...this.totallNum(this.priceLessThanFourty)]
+            },
+
+            sizeSelectionCount() {
+                return this.products.map(product => ({
+                    brand:product.brand, length:product.sizes.length
+                }))
+            },
+
+            countedSizeSelection() {
+                return this.sizeSelectionCount.filter(product => {
+                    return product.length == this.getMaxLength(this.sizeSelectionCount)
+                })
+            },
+
+            getDistinctBrand() {
+                return [...new Set(this.countedSizeSelection.map(product => product.brand))];
+            }
         },
 
         methods: {
-            
+            getPrice(product) {
+                return product.priceR ? product.priceR : product.priceO
+            },
+
+            totallNum(arr) {
+                return arr.reduce((m, x) => m.set(x, (m.get(x) || 0) + 1), new Map());  
+            },
+
+            getMaxLength(arr) {
+                return arr.reduce((p, c) => p.length > c.length ? p : c).length;
+            }
         }
     }
 </script>
