ddd21e0bf404625385496954c732be8b35de0a2f
yxj0312 <yanxiajun@hotmail.com>
2020-02-23 23:31:35 +0100

    Remove unused import in main.js

--- src/main.js
+++ src/main.js
@@ -3,25 +3,14 @@ import VueRouter from 'vue-router'
 import App from './App.vue'
 import routes from './routes'
 import Vue2TouchEvents from 'vue2-touch-events'
-import VueFusionCharts from 'vue-fusioncharts';
-import FusionCharts from 'fusioncharts';
-import Charts from 'fusioncharts/fusioncharts.charts';
-import Bars from 'vuebars';
-import ECharts from 'vue-echarts';
-import 'echarts/lib/chart/bar';
-import 'echarts/lib/component/tooltip';
 import Chartkick from 'vue-chartkick'
 import Chart from 'chart.js'
 
 
 Vue.use(VueRouter);
 Vue.use(Vue2TouchEvents);
-Vue.use(VueFusionCharts, FusionCharts, Charts);
-Vue.use(Bars)
 Vue.use(Chartkick.use(Chart))
 Vue.config.productionTip = false
-
-Vue.component('v-chart', ECharts)
 
 import './assets/sass/app.scss';
 
