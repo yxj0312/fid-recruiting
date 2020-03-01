1ae325e7583db6ee3bf96b2b09416a16a8127fd6
yxj0312 <yanxiajun@hotmail.com>
2020-03-01 20:48:04 +0100

    Remove pacakge usage

--- src/main.js
+++ src/main.js
@@ -3,13 +3,9 @@ import VueRouter from 'vue-router'
 import App from './App.vue'
 import routes from './routes'
 import Vue2TouchEvents from 'vue2-touch-events'
-import Chartkick from 'vue-chartkick'
-import Chart from 'chart.js'
-
 
 Vue.use(VueRouter);
 Vue.use(Vue2TouchEvents);
-Vue.use(Chartkick.use(Chart))
 Vue.config.productionTip = false
 
 import './assets/sass/app.scss';
