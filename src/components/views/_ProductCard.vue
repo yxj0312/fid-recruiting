027910b6ef874bac90292bbaf2dcb9000ed0f3e1
yxj0312 <yanxiajun@hotmail.com>
2020-04-23 23:17:28 +0200

    Reset productCard changes

--- src/components/views/_ProductCard.vue
+++ src/components/views/_ProductCard.vue
@@ -1,27 +1,9 @@
 <template>
     <div class="grid-cell grid-cell--border">
         <div class="product-card">
-        <div class="product-image" >
+        <div class="product-image">
             <a :href="product.url" target="_blank">
-                <!-- <img
-                    :src="image"
-                    @mouseover="changeImage"
-                    @mouseout="changeImage"
-                    :alt="product.description"> -->
-                
-      
-                   <!-- <img
-                    :src="changed"
-                    v-touch:touchhold="enterImage"
-                    v-touch:end="leaveImage"
-                    @mouseleave="leaveImage"
-                    @mouseenter="enterImage"
-                    :alt="product.description"
-                    :key="renderkey"
-                    > -->
-          
-                
-                <!-- <img
+                <img
                     :src="image"
                     v-if="!hover"
                     v-touch:touchhold="changeImage"
@@ -62,14 +44,11 @@
         },
 
         computed: {
-            // image() {
-            //     return this.product.images[0]
-            // },
-            // image2() {
-            //     return this.product.images[1]
-            // },
-            changed() {
-               return  this.hover ? this.product.images[0] : this.product.images[1]
+            image() {
+                return this.product.images[0]
+            },
+            image2() {
+                return this.product.images[1]
             }
         },
 
@@ -80,12 +59,6 @@
                     this.hover = !this.hover;
                 }, 300);
             },
-        },
-
-        watch: {
-            changed() {
-                this.renderkey++
-            }
         },
 
     }
